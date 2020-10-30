0bded9a4104884ace79fa0079242dcf1846932ce
Brian Martin Jaramillo Hernandez <59811590+Morfox71@users.noreply.github.com>
2020-10-29 23:00:32 -0600

    Mejora_de_ventanas

---
+++ Mediguide2/app/src/main/java/com/example/mediguide/Terminos_Condiciones.java
@@ -0,0 +1,25 @@
+package com.example.mediguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class Terminos_Condiciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terminos__condiciones);
+
+        DisplayMetrics medidasVentana = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(medidasVentana);
+
+        int ancho = medidasVentana.widthPixels;
+        int alto = medidasVentana.heightPixels;
+
+        getWindow().setLayout((int)(ancho * 0.85),(int)(alto * 0.53));
+        
+
+    }
+}
